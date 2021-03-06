1950e7e1eac5d057a36ecff805a5c591151b3efc
218252005 <typhigov20@gmail.com>
2021-03-06 21:52:30 +0200

    Moved a bracket in User.java

--- User.java
+++ User.java
@@ -10,7 +10,7 @@ public class User
     private String password;
 
     public User(String username, String password) 
-    {
+     {
         this.username = username;
         this.password = password;
     }
